0858f5109d4e74371ed390c3a6d8ef80e71e2080
Alberto <albertogilsanjuan@gmail.com>
2025-03-24 15:27:00 +0000

    ADD: Controller & Route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Resource;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource([
+    'activities', Resource::class
+]);
