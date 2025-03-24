c0dfb454cca2afb7871a9ea5f1f6f506530f7d0c
Alberto <albertogilsanjuan@gmail.com>
2025-03-24 15:31:42 +0000

    FIX: Route

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource([
-    'activities', Resource::class
-]);
+Route::resource('activities', Resource::class);
