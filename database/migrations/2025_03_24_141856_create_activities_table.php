bdd280a5bbccf2c6819532eaac4f3ed8e673ee93
Alberto <albertogilsanjuan@gmail.com>
2025-03-24 14:46:21 +0000

    ADD: Activity Table

---
+++ database/migrations/2025_03_24_141856_create_activities_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', ['surf', 'windsurf', 'layak', 'atv', 'hot air ballon']);
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->dateTime('datetime');
+            $table->boolean('paid')->default(0);
+            $table->integer('satisfaction')->nullable()->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activities');
+    }
+};
